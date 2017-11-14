af7fd1a2e1001226902ba81b1fcff3dadd657e05
Ryan Hageman <improv42@gmail.com>
2017-11-14 09:46:24 -0600

    Add abilty to update dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,4 +1,5 @@
 class DogsController < ApplicationController
+  before_action :set_dog, only: [:show, :edit, :update]
 
   def index
     @dogs = Dog.all
@@ -22,13 +23,30 @@ class DogsController < ApplicationController
   end
 
   def show
-    @dog = Dog.find(params[:id])
+
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @dog.update(dog_params)
+      redirect_to dog_url(@dog), notice: 'Successfully Updated'
+    else
+      flash.now[:notice] = "Something went wrong, we couldn't update #{@dog.name}"
+      render :edit
+    end
   end
 
   private
 
   def dog_params
     params.require(:dog).permit(:name, :age, :breed)
+  end
+
+  def set_dog
+    @dog = Dog.find(params[:id])
   end
 
 end
